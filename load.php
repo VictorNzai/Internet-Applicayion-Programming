4fa36111abcdda65ef92022d95d44964cc4ab441
Victorurl <166705594+VictorNzai@users.noreply.github.com>
2024-09-22 07:20:40 +0300

    Created The signup Method

--- load.php
+++ load.php
@@ -1,12 +1,13 @@
-<!-- <?php
+<?php
 
-
+require "includes/constants.php";
+require "includes/dbConnection.php";
 
 // Class Auto Load 
 
 function classAutoLoad($classname){
 
-    $directories = ["contents", "layouts", "menus"];
+    $directories = ["contents", "layouts", "menus", "forms", "processes"];
 
     foreach($directories AS $dir){
         $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
@@ -23,66 +24,11 @@ spl_autoload_register('classAutoLoad');
     $ObjMenus = new menus();
     $ObjHeadings = new headings();
     $ObjCont = new contents();
-
-require "includes/constants.php";
-require "includes/dbConnection.php";
-
-$conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
+    $ObjForm = new user_forms();
+    $conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
 
 
-// print 
-// print "<br>";
-// print "<br>";
-// print $_SERVER["PHP_SELF"];
-// print "<br>";
-// print "<br>";
-// print basename($_SERVER["PHP_SELF"]);
-// print "<br>";
-// print "<br>";
-// if(file_exists("index.php") AND is_readable("index.php")){
-//     print "yes";
-// }else{
-//     print "no";
-// }
+// Create process instances
 
-// Class Auto Load 
-
-// function classAutoLoad($classname){
-
-//     $directories = ["contents", "layouts", "menus"];
-
-//     foreach($directories AS $dir){
-//         $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
-//         if(file_exists($filename) AND is_readable($filename)){
-//             require_once $filename;
-//         }
-//     }
-// }
-
-// spl_autoload_register('classAutoLoad');
-
-// // Create instances of all classes
-//     $ObjLayouts = new layouts();
-//     $ObjMenus = new menus();
-//     $ObjHeadings = new headings();
-//     $ObjCont = new contents();
-
-
-
-
-
-
-// print 
-// print "<br>";
-// print "<br>";
-// print $_SERVER["PHP_SELF"];
-// print "<br>";
-// print "<br>";
-// print basename($_SERVER["PHP_SELF"]);
-// print "<br>";
-// print "<br>";
-// if(file_exists("index.php") AND is_readable("index.php")){
-//     print "yes";
-// }else{
-//     print "no";
-// } -->
+    $ObjAuth = new auth();
+    $ObjAuth->signup($conn);
